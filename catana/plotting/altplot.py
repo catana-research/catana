e9f69b7f04d164750b3678462d496b223533375c
catana-research <38889032+catana-research@users.noreply.github.com>
2019-11-17 20:31:29 +0000

    Fix readme.

---
+++ catana/plotting/altplot.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Altair plotting
+
+Provides wrapper functionality to easily utilise altair plots.
+"""
+
+def f():
+    return 1
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
